38a4bbc2cf4701a16956e001d0ff368907b20a53
Parth Mangtani <impmmmm@gmail.com>
2023-10-22 08:53:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# squareComponents
+
+reuseable react components for personal use.
---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as CustomSnackbar } from "./components/CustomSnackbar";
+export type { CustomSnackbarStateType } from "./components/CustomSnackbar";
+export { default as PasswordInput } from "./components/PasswordInput";
+export { default as ThemeToggle } from "./components/ThemeToggle";
