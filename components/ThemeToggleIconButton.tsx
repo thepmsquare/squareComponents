dcfabf15a63c58628a94594257af7ba45be1dc05
Parth Mangtani <impmmmm@gmail.com>
2023-12-31 16:04:32 +0530

    - add new component ThemeToggleIconButton.

---
+++ components/ThemeToggleIconButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  IconButton,
+  ButtonOwnProps,
+  IconButtonOwnProps,
+  Tooltip,
+} from "@mui/material";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+
+const ThemeToggleIconButton = (props: {
+  themeState: "dark" | "light";
+  customChangeThemeState: (newThemeState: "dark" | "light") => void;
+  color?: ButtonOwnProps["color"];
+  size?: IconButtonOwnProps["size"];
+}) => {
+  return (
+    <IconButton
+      id="toggle-theme-button"
+      size={props.size}
+      color={props.color}
+      onClick={() => props.customChangeThemeState(props.themeState)}
+    >
+      {props.themeState === "dark" ? (
+        <Tooltip title="change to dark mode.">
+          <LightModeIcon fontSize={props.size} />
+        </Tooltip>
+      ) : (
+        <Tooltip title="change to light mode.">
+          <DarkModeIcon fontSize={props.size} />
+        </Tooltip>
+      )}
+    </IconButton>
+  );
+};
+export default ThemeToggleIconButton;
