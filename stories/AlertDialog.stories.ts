3c3905841aca379e12ad1dcdafe4016280afe4f3
thepmsquare <impmmmm@gmail.com>
2025-03-12 12:26:55 +0530

    Update AlertDialog.stories.ts
    add args

--- stories/AlertDialog.stories.ts
+++ stories/AlertDialog.stories.ts
@@ -4,7 +4,13 @@ export default {
   title: "AlertDialog",
   component: AlertDialog,
   args: {
-    
+    open: true,
+    handleClose: () => void,
+    title: "confirm?",
+    handleSuccess: () => void,
+    confirmButtonColor: "error",
+    text: "description",
+    isLoading: false
   },
 };
 
